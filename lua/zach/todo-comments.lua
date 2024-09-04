afa125fdbe2659a38527aabf6ad076c1211e3447
Zachary Siegel <siegelzc@gmail.com>
2024-09-04 11:45:15 -0500

    extended todo-comments config

--- lua/zach/todo-comments.lua
+++ lua/zach/todo-comments.lua
@@ -4,16 +4,22 @@ vim.keymap.set("n", "<leader>cxn", todo_comments.jump_next)
 vim.keymap.set("n", "<leader>cxN", todo_comments.jump_prev)
 
 -- NOTE: Any updates to these keywords should be mirrored in the plugin configuration
-local FIX_keywords = {"fix", "FIX", "FIXME", "BUG", "FIXIT", "ISSUE"}
-vim.keymap.set("n", "<leader>cfn", function()
+local FIX_keywords = {"fix", "FIX", "FIXME", "bug", "BUG", "FIXIT", "ISSUE"}
+vim.keymap.set("n", "<leader>cfn", function() -- f(ix)
     todo_comments.jump_next({ keywords = FIX_keywords })
 end)
 vim.keymap.set("n", "<leader>cfN", function()
     todo_comments.jump_prev({ keywords = FIX_keywords })
 end)
+vim.keymap.set("n", "<leader>cbn", function() -- b(ug)
+    todo_comments.jump_next({ keywords = FIX_keywords })
+end)
+vim.keymap.set("n", "<leader>cbN", function()
+    todo_comments.jump_prev({ keywords = FIX_keywords })
+end)
 
 local TODO_keywords = {"todo", "TODO"}
-vim.keymap.set("n", "<leader>ctn", function()
+vim.keymap.set("n", "<leader>ctn", function() -- t(odo)
     todo_comments.jump_next({ keywords = TODO_keywords })
 end)
 vim.keymap.set("n", "<leader>ctN", function()
@@ -21,7 +27,7 @@ vim.keymap.set("n", "<leader>ctN", function()
 end)
 
 local WARN_keywords = {"warn", "WARN", "WARNING", "XXX"}
-vim.keymap.set("n", "<leader>cwn", function()
+vim.keymap.set("n", "<leader>cwn", function() -- w(arning)
     todo_comments.jump_next({ keywords = WARN_keywords })
 end)
 vim.keymap.set("n", "<leader>cwN", function()
@@ -29,18 +35,24 @@ vim.keymap.set("n", "<leader>cwN", function()
 end)
 
 local PERF_keywords = {"perf", "PERF", "OPTIM", "PERFORMANCE", "OPTIMIZE"}
-vim.keymap.set("n", "<leader>cpn", function()
+vim.keymap.set("n", "<leader>cpn", function() -- p(erformance)
     todo_comments.jump_next({ keywords = PERF_keywords })
 end)
 vim.keymap.set("n", "<leader>cpN", function()
     todo_comments.jump_prev({ keywords = PERF_keywords })
 end)
 
-local NOTE_keywords = {"note", "NOTE", "INFO"}
-vim.keymap.set("n", "<leader>cnn", function()
+local NOTE_keywords = {"note", "NOTE", "info", "INFO"}
+vim.keymap.set("n", "<leader>cnn", function() -- n(ote)
     todo_comments.jump_next({ keywords = NOTE_keywords })
 end)
 vim.keymap.set("n", "<leader>cnN", function()
+    todo_comments.jump_prev({ keywords = NOTE_keywords })
+end)
+vim.keymap.set("n", "<leader>cin", function() -- i(nfo)
+    todo_comments.jump_next({ keywords = NOTE_keywords })
+end)
+vim.keymap.set("n", "<leader>ciN", function()
     todo_comments.jump_prev({ keywords = NOTE_keywords })
 end)
 
