5fe9d6214bc1d978384809fb42b8fad7bcbedbeb
Zachary Siegel <zachary@foreflight.com>
2025-03-04 18:02:29 -0600

    remove textwidth assignment

--- lua/zach/wrap.lua
+++ lua/zach/wrap.lua
@@ -3,8 +3,8 @@ local function set_wrap_settings()
     vim.opt_local.wrap = true
     vim.opt_local.linebreak = true
     vim.opt_local.list = false
-    vim.opt_local.textwidth = 0
     vim.opt_local.wrapmargin = 0
+    vim.opt_local.formatoptions = 'cqj'
 end
 
 -- Set for markdown and text files
