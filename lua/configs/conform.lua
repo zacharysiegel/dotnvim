e3d01bd83b362bc64672ab3a95621982d5d82b16
zacharysiegel <siegelzc@gmail.com>
2025-09-06 11:23:54 -0500

    disable format on save

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -11,11 +11,11 @@ local options = {
     yaml = { "yamlfmt" }
   },
 
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
+  -- format_on_save = {
+  --   -- These options will be passed to conform.format()
+  --   timeout_ms = 500,
+  --   lsp_fallback = true,
+  -- },
 }
 
 return options
